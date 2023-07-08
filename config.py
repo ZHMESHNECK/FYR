7629966e647e7fb476aab32ab90ac895ccffc7d7
ZHMESHNECK <johndoe@example.com>
2023-07-08 11:56:51 +0300

    Version 1.4

---
+++ config.py
@@ -0,0 +1,2 @@
+city_riel = ['/kiev/', '/odessa/', '/dnepr/', '/lvov/', '/harkov/', '/ivano-frankovsk/', '/hmelnitskij/', '/sumy/', '/nikolaev/', '/zaporozhje/', '/rovno/',
+                  '/chernigov/', '/vinnitsa/', '/chernovtsy/', '/ternopol/', '/poltava/', '/cherkassy/', '/zhitomir/', '/skyi/', 'kropyvnytskyi/', '/uzhgorod/', '/lutsk/', '/kherson/']
--- rieltor_par.py
+++ rieltor_par.py
@@ -3,6 +3,74 @@ from bs4 import BeautifulSoup
 import requests
 
 
+# заготовка под генерацию ссылки с параметрами
+"""
+from bs4 import BeautifulSoup
+with open('rieltor.html', 'r', encoding='utf-8') as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, 'lxml')
+
+
+room = ['1-room', '2-rooms', '3-rooms', '4-rooms']
+rooms = ['rooms[0]=1', 'rooms[1]=2', 'rooms[2]=3']
+city = [i.attrs['data-index-url']
+        for i in soup.find_all('div', class_='nav_item_option_geo_city js_nav_input')] # взять города из config
+
+
+parametrs = {
+    'rooms': room[0],
+    'price_min': 'price_min=4000',
+    'price_max': '&price_max=9000',
+    'city': city[0],
+    'floor_min': '',
+    'floor_max': '',
+    'sort': 'byprice'
+}
+
+roomes = []
+c_room = int(input('Кол. комнат: '))
+while c_room != 0:
+    roomes.append(c_room)
+    c_room = int(input('Кол. комнат: '))
+roomes = list(set(roomes))
+mn_price = int(input('Мин: '))
+mx_price = int(input('Мах: '))
+mn_floor = int(input('ЭМин: '))
+mx_floor = int(input('Эmax: '))
+
+if len(roomes) == 1:
+    parametrs['rooms'] = room[roomes[0]-1] + '/?'
+else:
+    list_of_room = []
+    for i in roomes:
+        list_of_room.append(rooms[i-1])
+
+    parametrs['rooms'] = '?' + '&'.join(list_of_room) + '&'
+
+if mn_price != 0:
+    parametrs['price_min'] = f'price_min={mn_price}'
+if mx_price != 0:
+    parametrs['price_max'] = f'&price_max={mx_price}'
+
+if mn_floor != 0:
+    parametrs['floor_min'] = f'&floor_min={mn_floor}'
+if mx_floor != 0:
+    parametrs['floor_max'] = f'&floor_max={mx_floor}'
+
+gen_of_link = f'{parametrs["rooms"]}{parametrs["price_min"]}{parametrs["price_max"]}{parametrs["floor_min"]}{parametrs["floor_max"]}&sort={parametrs["sort"]}'
+
+
+link = f'https://rieltor.ua{parametrs["city"]}flats-rent/{gen_of_link}'
+
+print('https://rieltor.ua/kiev/flats-rent/2-rooms/?price_min=4000&price_max=9000&sort=byprice')
+print(
+    'https://rieltor.ua/kiev/flats-rent/?rooms[0]=1&rooms[1]=2&price_min=4000&price_max=9000&sort=byprice')
+print(
+    'https://rieltor.ua/rovno/flats-rent/?price_min=4000&price_max=9000&sort=-default&floor_min=6&floor_max=9&rooms[0]=1&rooms[1]=2')
+print(link)
+"""
+
 def call_data_rieltor():
 
     ua = UserAgent()
