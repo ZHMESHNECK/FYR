73d91be9690536ca2abf41b76bae3ca1c900ba89
ZHMESHNECK <johndoe@example.com>
2023-08-11 11:14:54 +0300

    ПР

--- bot/db/schemas/dbb.py
+++ bot/db/schemas/dbb.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Column, BigInteger, String, sql, Integer, DateTime
 from typing import List
 from gino import Gino
 import sqlalchemy as sa
@@ -19,3 +20,18 @@ class BaseModel(db.Model):
         values_str = " ".join(
             f"{name}={value!r}" for name, value in values.items())
         return f"<{model} {values_str}>"
+
+
+class User_model(BaseModel):
+    __tablename__ = 'users'
+    user_id = Column(BigInteger, primary_key=True, nullable=False)
+    city = Column(String(25), nullable=True)
+    min_price = Column(BigInteger, nullable=True)
+    max_price = Column(BigInteger, nullable=True)
+    count_rooms = Column(String(3), nullable=True)
+    min_floor = Column(Integer, nullable=True)
+    max_floor = Column(Integer, nullable=True)
+    sort = Column(String(25), nullable=True)
+    time = Column(DateTime(timezone=False))
+
+    query: sql.select()
